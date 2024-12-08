1094c5f510289ba5ba9066bfc0b1bc158332ec54
Sergey87 <serge.techno@gmail.com>
2024-12-09 00:48:19 +0300

    finished portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
 import './Portfolio.css';
+import { motion, useInView, useScroll, useTransform } from 'motion/react';
 
 const items = [
   {
@@ -38,35 +40,137 @@ const items = [
   },
 ];
 
+const imgVariants = {
+  initial: {
+    x: -500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const textVariants = {
+  initial: {
+    x: 500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: 'easeInOut',
+      staggerChildren: 0.05,
+    },
+  },
+};
+
 const ListItem = ({ item }) => {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: '-100px' });
   return (
-    <div className='pItem'>
-      <div className='pImg'>
+    <div className='pItem' ref={ref}>
+      <motion.div
+        className='pImg'
+        variants={imgVariants}
+        animate={isInView ? 'animate' : 'initial'}
+      >
         <img src={item.img} alt='img pic' />
-      </div>
-      <div className='pText'>
-        <h1>{item.title}</h1>
-        <p>{item.desc}</p>
-        <a href={item.link}>
+      </motion.div>
+      <motion.div
+        className='pText'
+        variants={textVariants}
+        animate={isInView ? 'animate' : 'initial'}
+      >
+        <motion.h1 variants={textVariants}>{item.title}</motion.h1>
+        <motion.p variants={textVariants}>{item.desc}</motion.p>
+        <motion.a variants={textVariants} href={item.link}>
           <button>View Project</button>
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </div>
   );
 };
 
 const Portfolio = () => {
+  const [containerDistance, setContainerDistance] = useState(0);
+  const ref = useRef();
+
+  useEffect(() => {
+    const calculateDistance = () => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        setContainerDistance(rect.left);
+      }
+    };
+
+    calculateDistance();
+
+    window.addEventListener('resize', calculateDistance);
+
+    return () => {
+      window.removeEventListener('resize', calculateDistance);
+    };
+  }, []);
+
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const xTranslate = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, -window.innerWidth * items.length]
+  );
   return (
-    <div className='portfolio'>
-      <div className='pList'>
+    <div className='portfolio' ref={ref}>
+      <motion.div className='pList' style={{ x: xTranslate }}>
+        <div
+          className='empty'
+          style={{
+            width: window.innerWidth - containerDistance,
+            // backgroundColor: 'pink',
+          }}
+        ></div>
         {items.map((item) => (
           <ListItem item={item} key={item.id} />
         ))}
+      </motion.div>
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <div className='pProgress'>
+        <svg width='100%' height='100%' viewBox='0 0 160 160'>
+          <circle
+            cx='80'
+            cy='80'
+            r='70'
+            fill='none'
+            stroke='#ddd'
+            strokeWidth={20}
+          />
+          <motion.circle
+            cx='80'
+            cy='80'
+            r='70'
+            fill='none'
+            stroke='#dd4c62'
+            strokeWidth={20}
+            style={{ pathLength: scrollYProgress }}
+            transform='rotate(-90 80 80)'
+          />
+        </svg>
       </div>
-      <section />
-      <section />
-      <section />
-      <section />
     </div>
   );
 };
